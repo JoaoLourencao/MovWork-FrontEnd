8ba5385fbd79d263c23a4fb86f3d891e5c2eafd7
unknown <jotap.lourencao@gmail.com>
2021-02-23 22:31:59 -0300

    progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ProgressBar from './components/ProgressBar';
+import {ProgressBar} from './components/ProgressBar';
 import './styles/global.css';
 
 function App() {
   return (
-    <div>    
+    <div className='container'>    
       <ProgressBar/>  
     </div>
   );
